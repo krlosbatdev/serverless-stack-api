b2396b0de19e6694c8cc01ac33141e4eab61145b
Kar <krlosbatdev@gmail.com>
2019-12-26 17:08:58 -0500

    Fixing the mistake

--- create.js
+++ create.js
@@ -1,9 +1,11 @@
 import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import {
+  success,
+  failure
+} from "./libs/response-lib";
 
 export async function main(event, context) {
-  uuid.abc.gibberish;
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
@@ -20,6 +22,8 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    return failure({ status: false });
+    return failure({
+      status: false
+    });
   }
 }
